610326153f43e8f19ae1428da7eeafb282b5d243
plumsun <plumsun@163.com>
2024-09-11 17:47:00 +0800

    fix: maven 打包优化

--- src/main/java/com/example/short_chains/utils/SortGenerationUtils.java
+++ src/main/java/com/example/short_chains/utils/SortGenerationUtils.java
@@ -3,6 +3,7 @@ package com.example.short_chains.utils;
 import cn.hutool.core.codec.Base62;
 import com.google.common.hash.Hashing;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +14,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class SortGenerationUtils {
 
+    @Value("${system.addr}")
+    private String addr;
+
     public String generation(String url) {
         byte[] bytes = Hashing.murmur3_32_fixed().hashUnencodedChars(url).asBytes();
         String encode = Base62.encode(bytes);
         ServerProperties properties = new ServerProperties();
-        String sortUrl = "http://localHost:" + properties.getPort() + "/" + encode;
+        // NOTE: 17:27 此处链接写死，未来会通过数据库维护
+        String sortUrl = "http://" + addr + ":" + properties.getPort() + "/" + encode;
         log.info("长链：{}，初始短链生成成功:{}", url, sortUrl);
         return sortUrl;
     }
